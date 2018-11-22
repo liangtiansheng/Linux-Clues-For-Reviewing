757881d6cea3470e59f63fe5c0b4945d0b94549c
亮天生 <2780715822@qq.com>
2018-11-22 15:20:15 +0800

    v

--- k8s专题/k8s高可用专题/k8s kubeadm_ha.bash
+++ k8s专题/k8s高可用专题/k8s kubeadm_ha.bash
@@ -423,6 +423,35 @@ EOF
     ;; MSG SIZE  rcvd: 149
 
     [root@centos-deploy-69755846fd-rmmlp /]#
+    root@master2:~# kubectl exec -it centos-deploy-69755846fd-9nxl4 bash
+    [root@centos-deploy-69755846fd-9nxl4 /]# curl nginx.default.svc.cluster.local.
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Welcome to nginx!</title>
+    <style>
+        body {
+            width: 35em;
+            margin: 0 auto;
+            font-family: Tahoma, Verdana, Arial, sans-serif;
+        }
+    </style>
+    </head>
+    <body>
+    <h1>Welcome to nginx!</h1>
+    <p>If you see this page, the nginx web server is successfully installed and
+    working. Further configuration is required.</p>
+
+    <p>For online documentation and support please refer to
+    <a href="http://nginx.org/">nginx.org</a>.<br/>
+    Commercial support is available at
+    <a href="http://nginx.com/">nginx.com</a>.</p>
+
+    <p><em>Thank you for using nginx.</em></p>
+    </body>
+    </html>
+    [root@centos-deploy-69755846fd-9nxl4 /]#
+20、关闭vip所在的节点，发现vip飘到了master2
 
 
 
