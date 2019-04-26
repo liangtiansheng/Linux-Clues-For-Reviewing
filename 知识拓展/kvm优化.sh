a01d53262608e63080ff735ca72c41498ef330a0
liangtiansheng <1062817308@qq.com>
2019-04-26 17:23:46 +0800

    v

---
+++ 知识拓展/kvm优化.sh
@@ -0,0 +1,6 @@
+KVM CPU优化(http://blog.csdn.net/dylloveyou/article/details/71169463)
+KVM 内存优化(http://blog.csdn.net/dylloveyou/article/details/71338378)
+KVM 磁盘IO优化(http://blog.csdn.net/dylloveyou/article/details/71515880)
+KVM 网络优化(https://blog.csdn.net/dylloveyou/article/details/72382079)
+
+
