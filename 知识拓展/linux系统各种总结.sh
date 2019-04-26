47ac83f3c25283d099c11670caa7ee8b954b00f1
liangtiansheng <1062817308@qq.com>
2019-04-26 16:58:06 +0800

    v

---
+++ 知识拓展/linux系统各种总结.sh
@@ -0,0 +1,18 @@
+http://www.cnblogs.com/f-ck-need-u/p/7048359.html#auto_id_6
+
+1.Linux基础
+2.Shell脚本、bash特性系列
+3.shell高效率处理文本
+4.Linux基本服务
+5.openssl系列
+6.杂项内容
+7.我的个人翻译
+    1.翻译：grub2详解(翻译和整理官方手册)
+    2.翻译：Bios boot partition
+    3.翻译：man ssh(ssh命令中文手册)
+    10.翻译：man rpcbind(rpcbind中文手册)
+    13.翻译：man statd(rpc.statd中文手册)
+    16.翻译：man getopt(1)中文手册
+
+
+
